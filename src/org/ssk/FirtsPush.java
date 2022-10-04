5e207ad8bc83fbc2d73ad291e90272d7fff80dbb
Shank013 <sankarkumar.sam@gmail.com>
2022-10-04 19:37:42 +0530

    SK

---
+++ src/org/ssk/FirtsPush.java
@@ -0,0 +1,13 @@
+package org.ssk;
+
+public class FirtsPush {
+
+	private void collegeDetails() {
+		System.out.println("College Name: KCG");
+		}
+	
+	public static void main(String[] args) {					
+		FirtsPush k = new FirtsPush();
+		k.collegeDetails();
+	}
+}
